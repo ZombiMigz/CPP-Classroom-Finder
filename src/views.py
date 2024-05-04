8bffc272184b7a50ffb030263ab404ec958f64e4
Phillip Che <pche@cpp.edu>
2024-05-03 23:21:33 -0700

    fix lint issues

--- src/views.py
+++ src/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from src.models import ScheduleBlock
 from datetime import datetime 
 
+
 def index(request: HttpRequest):
     classes = ScheduleBlock.objects.all()
     date = datetime.now()
-    return render(request, 'index.html', {"classes": classes, "date": date })
+    return render(request, 'index.html', {"classes": classes, "date": date})
 
 
