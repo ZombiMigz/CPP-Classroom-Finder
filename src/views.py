7cd4fc77540a4a8294b31586b711ed3b674cc0c9
Nil Patel <nilrnp@gmail.com>
2024-05-04 12:46:33 -0700

    initial ui setup

--- src/views.py
+++ src/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from src.forms import ClassroomForm
 from src.models import ScheduleBlock
 from datetime import datetime
 
 
 def index(request: HttpRequest):
+    form = ClassroomForm()
+    return render(request, 'index.html', {'form': form})
     classes = ScheduleBlock.objects.all()
     date = datetime.now()
     return render(request, 'index.html', {"classes": classes, "date": date})
