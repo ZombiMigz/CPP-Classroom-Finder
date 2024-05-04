a3ac41dfb08366abec6ee3e6d2913049748124b8
Phillip Che <pche@cpp.edu>
2024-05-03 23:23:09 -0700

    lint

--- src/views.py
+++ src/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 from src.models import ScheduleBlock
-from datetime import datetime 
+from datetime import datetime
 
 
 def index(request: HttpRequest):
     classes = ScheduleBlock.objects.all()
     date = datetime.now()
     return render(request, 'index.html', {"classes": classes, "date": date})
-
-
