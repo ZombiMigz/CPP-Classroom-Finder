4a7efdba684afe02e984286ffee6ef4a8c067ca7
Phillip Che <pche@cpp.edu>
2024-05-03 23:12:08 -0700

    fetch and display objects from database

--- src/views.py
+++ src/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse, HttpRequest
-
+from django.http import HttpRequest
+from django.shortcuts import render
+from src.models import ScheduleBlock
+from datetime import datetime 
 
 def index(request: HttpRequest):
-    return HttpResponse("Hello, world!")
+    classes = ScheduleBlock.objects.all()
+    date = datetime.now()
+    return render(request, 'index.html', {"classes": classes, "date": date })
+
+
